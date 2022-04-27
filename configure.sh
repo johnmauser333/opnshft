4633b80067584a45037a4ee8e51a74c488d16172
johnmauser333 <86488300+johnmauser333@users.noreply.github.com>
2022-04-28 02:10:42 +1000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,32 @@
 #!/bin/sh
 # V2Ray new configuration
 cat <<-EOF > /etc/v2ray/config.json
-$CONFIG_JSON
+{
+    "inbounds": [
+        {
+            "port": 8080,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "ac7508a0-4bb4-4ba1-b30e-cc3b7df6558e",
+                        "security": "chacha20-poly1305",
+                        "alterId": 0
+                    }
+                ],
+                "disableInsecureEncryption": true
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
 EOF
 # Run V2Ray
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
