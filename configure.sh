966cad369a64c1996576d66e4feb2e3094f67b01
johnmauser333 <86488300+johnmauser333@users.noreply.github.com>
2022-04-28 02:36:08 +1000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -17,7 +17,7 @@ cat <<-EOF > /etc/v2ray/config.json
                 "disableInsecureEncryption": true
             },
             "streamSettings": {
-                "network": "ws"
+                "network": "tcp"
             }
         }
     ],
