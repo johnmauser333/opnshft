020318062d9762a7b1a7a8a1392c62dadc810b21
johnmauser333 <86488300+johnmauser333@users.noreply.github.com>
2022-04-28 15:01:56 +1000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,59 +4,28 @@ cat <<-EOF > /etc/xray/config.json
 {
     "inbounds": [
         {
-            "tag": "in_tomcat",
-            "port": 3737,
-            "protocol": "dokodemo-door",
-            "settings": {
-                "address": "127.0.0.1",
-                "port": 8080,
-                "network": "tcp"
-            }
-        },
-        {
-            "tag": "in_interconn",
             "port": 3333,
             "protocol": "vmess",
             "settings": {
                 "clients": [
                     {
                         "id": "4e87136b-ee03-4c23-b567-48c651cf3707",
-                        "alterId": 0,
-                        "security": "chacha20-poly1305"
+                        "security": "chacha20-poly1305",
+                        "alterId": 0
                     }
-                ]
+                ],
+                "disableInsecureEncryption": true
             },
             "streamSettings": {
-              "network": "ws"
+                "network": "ws"
             }
         }
     ],
-    "reverse": {
-        "portals": [
-            {
-                "tag": "portal",
-                "domain": "google.com"
-            }
-        ]
-    },
-    "routing": {
-        "rules": [
-            {
-                "type": "field",
-                "inboundTag": [
-                    "in_tomcat"
-                ],
-                "outboundTag": "portal"
-            },
-            {
-                "type": "field",
-                "inboundTag": [
-                    "in_interconn"
-                ],
-                "outboundTag": "portal"
-            }
-        ]
-    }
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
 }
 EOF
 # Run V2Ray
