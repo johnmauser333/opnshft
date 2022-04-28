1927717b8582f33385f108d44d44ce67fd7460e1
johnmauser333 <86488300+johnmauser333@users.noreply.github.com>
2022-04-28 22:27:09 +1000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,74 +4,28 @@ cat <<-EOF > /etc/xray/config.json
 {
     "inbounds": [
         {
-            "tag": "in_tomcat",
-            "port": 3737,
-            "protocol": "dokodemo-door",
-            "settings": {
-                "address": "127.0.0.1",
-                "port": 8080,
-                "followRedirect": true,
-                "network": "tcp"
-            },
-             "sniffing": {
-               "enabled": true,
-               "destOverride": ["http", "tls"]
-             }
-           
-        },
-        {
-            "tag": "in_interconn",
             "port": 3333,
             "protocol": "vmess",
             "settings": {
                 "clients": [
                     {
-                        "id": "4e87136b-ee03-4c23-b567-48c651cf3707",
-                        "alterId": 0,
-                        "security": "chacha20-poly1305"
+                        "id": "$UUID",
+                        "security": "chacha20-poly1305",
+                        "alterId": 0
                     }
-                ]
+                ],
+                "disableInsecureEncryption": true
             },
             "streamSettings": {
-              "network": "ws"
-            },
-            "wsSettings": {
-              "headers": {
-              "Host": "opnshft-tomcat-yoshimitsu737-dev.apps.sandbox.x8i5.p1.openshiftapps.com"
-              }
-            },
-            "sockopt": {
-              "mark": 0,
-              "tproxy": "tproxy"
+                "network": "ws"
             }
         }
     ],
-    "reverse": {
-        "portals": [
-            {
-                "tag": "portal",
-                "domain": "google.com"
-            }
-        ]
-    },
-    "routing": {
-        "rules": [
-            {
-                "type": "field",
-                "inboundTag": [
-                    "in_tomcat"
-                ],
-                "outboundTag": "portal"
-            },
-            {
-                "type": "field",
-                "inboundTag": [
-                    "in_interconn"
-                ],
-                "outboundTag": "portal"
-            }
-        ]
-    }
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
 }
 EOF
 # Run V2Ray
