117efe40a1c8d79ac8e97024bb550b6b9aae172b
johnmauser333 <86488300+johnmauser333@users.noreply.github.com>
2022-04-28 22:33:06 +1000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -9,7 +9,7 @@ cat <<-EOF > /etc/xray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "$UUID",
+                        "id": "d590d760-2c08-444f-af64-bb2dd24c0db8",
                         "security": "chacha20-poly1305",
                         "alterId": 0
                     }
